9a4d7914e2c3d2cfa827693bd176a345b337cc2b
foxiae <xapatheiax@gmail.com>
2016-03-23 21:12:44 -0400

    email acct activation not working

--- 7_rails/sample_app/lib/environments/production.rb
+++ 7_rails/sample_app/lib/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  host = "localhost:3000"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
